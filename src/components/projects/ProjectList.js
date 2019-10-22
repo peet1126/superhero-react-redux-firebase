7a5428d501b2082ddeeab40fc428b813c830e3d4
Peter McDonald <pbmcdonald7@gmail.com>
2019-10-22 16:21:39 -0500

    first commit and file setup

---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const ProjectList = () => {
+  return (
+    <div className="project-list section">
+      <div className="card z-depth-0 project-summary ">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by Tony Stark</p>
+          <p className="grey-text">October 22, 1:00am</p>
+        </div>
+      </div>
+      <div className="card z-depth-0 project-summary ">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by Tony Stark</p>
+          <p className="grey-text">October 22, 1:00am</p>
+        </div>
+      </div>
+      <div className="card z-depth-0 project-summary ">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by Tony Stark</p>
+          <p className="grey-text">October 22, 1:00am</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectList;
