8cfef0d774f91651625277c57a0bc3c3d2171358
Peter McDonald <petermcdonald@Peters-MacBook-Air.local>
2021-01-21 07:33:05 -0600

    added notifications

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import { firestoreConnect } from "react-redux-firebase";
 import { compose } from "redux";
 import { Redirect } from "react-router-dom";
+import moment from "moment";
 
 const ProjectDetails = props => {
   const { project, auth } = props;
@@ -21,7 +22,7 @@ const ProjectDetails = props => {
             <div>
               Posted by {project.authorFirstName} {project.authorLastName}
             </div>
-            <div>October 22, 3:00am</div>
+            <div>{moment(project.createAt.toDate()).calendar()}</div>
           </div>
         </div>
       </div>
--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,12 +1,13 @@
 import React from "react";
+import moment from "moment";
 
 const ProjectSummary = ({ project }) => {
   return (
     <div className="card z-depth-0 project-summary ">
       <div className="card-content grey-text text-darken-3">
         <span className="card-title">{project.title}</span>
-        <p>Posted by Tony Stark</p>
-        <p className="grey-text">October 22, 1:00am</p>
+        <p>Posted by {project.authorFirstName} {project.authorLastName}</p>
+        <p className="grey-text">{moment(project.createAt.toDate()).calendar()}</p>
       </div>
     </div>
   );
