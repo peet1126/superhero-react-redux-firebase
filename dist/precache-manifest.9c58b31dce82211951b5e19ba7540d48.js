5915349922fce21bfcb04ecae7c01cdac93a6df2
Peter McDonald <petermcdonald@Peters-MacBook-Air.local>
2021-01-21 09:16:13 -0600

    test

---
+++ dist/precache-manifest.9c58b31dce82211951b5e19ba7540d48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "311c61d495294a8a19cce954779fc6da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac9a6df6df97e9cba078",
+    "url": "/static/css/main.7ca0630e.chunk.css"
+  },
+  {
+    "revision": "c2c3537ed3d0bf9df77c",
+    "url": "/static/js/2.81394d70.chunk.js"
+  },
+  {
+    "revision": "dad1e10c751afc1e6fffeb8be63f9aab",
+    "url": "/static/js/2.81394d70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac9a6df6df97e9cba078",
+    "url": "/static/js/main.3648c7d8.chunk.js"
+  },
+  {
+    "revision": "fc21ac669133f829cda2",
+    "url": "/static/js/runtime-main.21ab92b4.js"
+  }
+]);
--- src/index.js
+++ src/index.js
@@ -15,32 +15,13 @@ import "firebase/firestore";
 import "firebase/auth";
 import { useSelector } from "react-redux";
 import { isLoaded } from "react-redux-firebase";
-// import { reduxFirestore } from "redux-firestore";
-// import { reactReduxFirebase, getFirebase } from "react-redux-firebase";
 
-// Testing this - it should be in fbConfig
-
-// // Your web app's Firebase configuration
-// const config = {
-//   apiKey: "AIzaSyAjzK4PL0sZ9YXkhCXxJ2M7ZdX7WiSdecI",
-//   authDomain: "react-redux-firebase-732ae.firebaseapp.com",
-//   databaseURL: "https://react-redux-firebase-732ae.firebaseio.com",
-//   projectId: "react-redux-firebase-732ae",
-//   storageBucket: "react-redux-firebase-732ae.appspot.com",
-//   messagingSenderId: "876900563357",
-//   appId: "1:876900563357:web:155c603dfb8844e7805774",
-//   measurementId: "G-9BLTMNH2YY"
-// };
-// // Initialize Firebase
-// firebase.initializeApp(config);
 
 const store = createStore(
   rootReducer,
   compose(
     applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
     reduxFirestore(fbConfig), // - v17 comments
-    // reduxFirestore(config)
-    // reactReduxFirebase(fbConfig)
   )
 );
 
